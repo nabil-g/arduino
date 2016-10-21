5d0a3eba2a01ca059296ec5ed12bf640d50f519e
Nabil Ghedjati <nabil.ghedjati@gmail.com>
2016-10-21 10:04:07 +0200

    update

--- sketch_pir/sketch_pir.ino
+++ sketch_pir/sketch_pir.ino
@@ -10,12 +10,7 @@ void setup() {
   pinMode(ledPin, OUTPUT);      // declare LED as output
   pinMode(inputPin, INPUT);     // declare sensor as input
   Serial.begin(9600);
-
-  Serial.print("calibrating sensor ");
-  for(int i = 0; i < calibrationTime; i++){
-    Serial.print(".");
-    delay(1000);
-  }
+  delay(30000);
 }
 
 void loop(){
@@ -27,7 +22,7 @@ void loop(){
 
     if (pirState == LOW) {
       // we have just turned on
-      Serial.println("Motion detected!");
+      //Serial.println("Motion detected!");
       // We only want to print on the output change, not state
       pirState = HIGH;
     }
@@ -36,7 +31,7 @@ void loop(){
     delay(300);
     if (pirState == HIGH){
       // we have just turned of
-      Serial.println("Motion ended!");
+      //Serial.println("Motion ended!");
       // We only want to print on the output change, not state
       pirState = LOW;
     }
